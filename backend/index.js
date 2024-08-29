70bf2dcbc27ad20e215438dbf34ddc00855ec4e2
Shanto57575 <shanto57575@gmail.com>
2024-08-29 22:19:38 +0600

    cors issues

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,17 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: true,
+    origin: 'https://stayzest-cbf59.web.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
