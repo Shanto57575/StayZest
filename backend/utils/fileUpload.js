43e8a6639d3022f9d2eff435edc286df6784700b
Shanto57575 <shanto57575@gmail.com>
2024-09-01 00:32:21 +0600

    deployment issues

--- backend/utils/fileUpload.js
+++ backend/utils/fileUpload.js
@@ -1,31 +1,3 @@
-// import multer from 'multer';
-// import path from 'path';
-// import fs from 'fs';
-
-// const uploadPath = path.join(path.dirname(''), 'uploads');
-// fs.mkdirSync(uploadPath, { recursive: true });
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, uploadPath);
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//     if (file && file.mimetype && file.mimetype.startsWith('image/')) {
-//         cb(null, true);
-//     } else {
-//         cb(new Error('Invalid file type, only images are allowed!'), false);
-//     }
-// };
-
-// const upload = multer({ storage, fileFilter });
-
-// export default upload;
-
 import multer from 'multer';
 
 const storage = multer.memoryStorage();
@@ -38,12 +10,11 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-// Configure multer to use memory storage
 const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
     limits: {
-        fileSize: 5 * 1024 * 1024, // Limit file size to 5MB
+        fileSize: 5 * 1024 * 1024,
     }
 });
 
